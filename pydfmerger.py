e5b112ef859afb543ce6e794c5f327eb03f9486c
sb <simon@burgr.io>
2023-07-21 10:13:11 +0200

    added inserting blank pages

--- pydfmerger.py
+++ pydfmerger.py
@@ -2,10 +2,11 @@ import os
 import platform
 import subprocess
 import tkinter as Tki
+from tempfile import mkdtemp
 from tkinter.filedialog import askopenfilenames, asksaveasfilename
 from tkinter.messagebox import askyesno
 
-from pypdf import PdfMerger
+from pypdf import PaperSize, PdfMerger, PdfWriter
 
 root = Tki.Tk()
 root.title("PyDFGenerator")
@@ -25,6 +26,30 @@ root.resizable(width=False, height=False)
 # Listbox
 my_listbox = Tki.Listbox(root)
 my_listbox.place(x=10, y=10, width=440, height=155)
+
+tempDir = None
+filePath = None
+
+
+def create_empty():
+    """creates a temporary file and adds an to it"""
+    global tempDir
+    if not tempDir:
+        tempDir = mkdtemp()
+    blankFilePath = tempDir + "/blank.pdf"
+    if not os.path.isfile(blankFilePath):
+        writer = PdfWriter()
+        writer.add_blank_page(PaperSize.A4.width, PaperSize.A4.height)
+        with open(blankFilePath, "wb") as output_stream:
+            writer.write(output_stream)
+        print(blankFilePath)
+    return blankFilePath
+
+
+def insert_empty_page():
+    """inserts an empty page with button press"""
+    blankFilePath = create_empty()
+    my_listbox.insert(Tki.END, blankFilePath)
 
 
 def merge_pdf():
@@ -97,19 +122,25 @@ def move_down():
 MoveUpButton = Tki.Button(root)
 MoveUpButton["justify"] = "center"
 MoveUpButton["text"] = "↑"
-MoveUpButton.place(x=460, y=30, width=70, height=25)
+MoveUpButton.place(x=460, y=10, width=70, height=25)
 MoveUpButton["command"] = move_up
 
 MoveDownButton = Tki.Button(root)
 MoveDownButton["justify"] = "center"
 MoveDownButton["text"] = "↓"
-MoveDownButton.place(x=460, y=70, width=70, height=25)
+MoveDownButton.place(x=460, y=50, width=70, height=25)
 MoveDownButton["command"] = move_down
+
+InsertEmptyPageButton = Tki.Button(root)
+InsertEmptyPageButton["justify"] = "center"
+InsertEmptyPageButton["text"] = "Insert Blank"
+InsertEmptyPageButton.place(x=460, y=90, width=70, height=25)
+InsertEmptyPageButton["command"] = insert_empty_page
 
 OpenFileButton = Tki.Button(root)
 OpenFileButton["justify"] = "center"
 OpenFileButton["text"] = "Add File"
-OpenFileButton.place(x=460, y=140, width=70, height=25)
+OpenFileButton.place(x=460, y=130, width=70, height=25)
 OpenFileButton["command"] = open_file
 
 SaveFileButton = Tki.Button(root)
